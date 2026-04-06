63ec274f1673ec9dcac5782f4614219ac9210ead
juhwan0930 <a01024496935@gmail.com>
2026-04-06 09:48:31 +0900

    헤더 수정

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 console.clear();
 
 $(".search_btn").click(function () {
-  $(".search_box, .search_btn, header, .top_bar_logo > a").toggleClass(
-    "active",
-  );
+  $(".search_box, .search_btn, header, .top_bar_logo").toggleClass("active");
 });
